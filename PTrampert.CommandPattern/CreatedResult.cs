b9f6100c414698983b1a9eb796f7c085bdb1c48a
Paul Trampert <paul.trampert@gmail.com>
2016-06-18 16:21:08 -0400

    Added generic result type for write/delete commands.

---
+++ PTrampert.CommandPattern/CreatedResult.cs
@@ -0,0 +1,17 @@
+﻿namespace PTrampert.CommandPattern
+{
+    public class CreatedResult<TEntity> : IWriteResult<TEntity>
+    {
+        public TEntity Entity { get; set; }
+
+        public CreatedResult(TEntity entity = default(TEntity))
+        {
+            Entity = entity;
+        }
+
+        public TReturn Handle<TReturn>(IWriteResultHandler<TEntity, TReturn> handler)
+        {
+            return handler.Created(Entity);
+        }
+    }
+}
---
+++ PTrampert.CommandPattern/IWriteResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTrampert.CommandPattern
+{
+    public interface IWriteResult<out TEntity>
+    {
+        TReturn Handle<TReturn>(IWriteResultHandler<TEntity, TReturn> handler);
+    }
+}
